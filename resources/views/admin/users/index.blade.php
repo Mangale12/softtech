bf2d1926747dd29a05881e14b3930ff2f25e74e1
mangale12 <mangaletamang65@gmail.com>
2024-08-14 21:24:19 +0545

    document view update

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -116,119 +116,132 @@
     </div>
 
     <div class="card">
-        <div class="card-body">
-            <table class="table" id="example-table" width="100%" cellspacing="0">
+        <div class="card-body" style="overflow-x: auto;">
+            <table class="table table-bordered table-hover" id="example-table" width="100%" cellspacing="0">
                 <thead class="thead-light">
                     <tr>
-                        <th colspan="2">No</th>
-                        <th colspan="2">Name</th>
-                        <th colspan="2">Email</th>
-                        <th colspan="2">Phone</th>
-                        <th colspan="2">Position</th>
-                        <th colspan="2">Member Type</th>
-                        <th colspan="2">Profile</th>
-                        <th colspan="2">Company Name</th>
-                        <th colspan="2">Founded year</th>
-                        <th colspan="2">Company Logo</th>
-                        <th colspan="2">Pan No</th>
-                        <th colspan="2">PAN Photo</th>
-                        <th colspan="2">Registration No</th>
-                        <th colspan="2">Company Register File</th>
-                        <th colspan="2">Tax Clearance Certificate</th>
-                        <th colspan="2">Is Approved</th>
-                        <th colspan="2">Applied at</th>
-                        <th colspan="2">Action</th>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Position</th>
+                        <th>Member Type</th>
+                        <th>Profile</th>
+                        <th>Company Name</th>
+                        <th>Founded Year</th>
+                        <th>Company Logo</th>
+                        <th>PAN No</th>
+                        <th>PAN Photo</th>
+                        <th>Registration No</th>
+                        <th>Company Register File</th>
+                        <th>Tax Clearance Certificate</th>
+                        <th>Is Approved</th>
+                        <th>Applied at</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @if($data)
-                    @foreach($data as $key => $row)
-                    @php
-                        $legal_documents = [];
-                        $company = [];
-                        if (!empty($user->member)) {
-                            $legal_documents = json_decode($user->member->legal_documents, true);
-                            $company = json_decode($user->member->company, true);
-
-                        }
-                        // dd($legal_documents);
-                    @endphp
-                    <tr>
-                        <td colspan="2">{{ $key + 1 }}</td>
-                        <td colspan="2">{{$row->name}} </td>
-                        <td colspan="2">{{ $row->email}}</td>
-                        <td colspan="2">{{$row->mobile}}</td>
-                        <td colspan="2">{{ !empty($row->member) ? $row->member->member_post : '' }}</td>
-                        <td colspan="2"> {{ $row->member ? ($row->member->memberType ? $row->member->memberType->title : '') : '' }}</td>
-                        <td colspan="2">
-                            @if(isset($row->avatar))
-                            <img src="{{ asset($row->avatar)}}" width="45px" />
-                            @endif
-                        </td>
-                        <td colspan="2">{{ !empty($company['company_name']) ? $company['company_name'] : '' }}</td>
-                        <td colspan="2">{{ !empty($company['company_founded_year']) ? $company['company_founded_year'] : '' }}</td>
-                        <td colspan="2">
-                            @if(!empty($company['company_logo']))
-                                {{-- @if($company['company_logo'] == null || $company['company_logo'] == '') --}}
-                                <img src="{{ asset($company['company_logo']) }}" alt="Company Logo" height="50" width="50">
+                    @forelse($data as $key => $row)
+                        @php
+                            $legal_documents = $row->member ? json_decode($row->member->legal_documents, true) : [];
+                            $company = $row->member ? json_decode($row->member->company, true) : [];
+                        @endphp
+                        <tr>
+                            <td>{{ $key + 1 }}</td>
+                            <td>{{ $row->name }}</td>
+                            <td>{{ $row->email }}</td>
+                            <td>{{ $row->mobile }}</td>
+                            <td>{{ $row->member->member_post ?? '' }}</td>
+                            <td>{{ $row->member->memberType->title ?? '' }}</td>
+                            <td>
+                                @if(isset($row->avatar))
+                                    <img src="{{ asset($row->avatar) }}" width="45px" class="img-thumbnail" data-toggle="modal" data-target="#imageModal" data-image="{{ asset($row->avatar) }}" />
+                                @endif
+                            </td>
+                            <td>{{ $company['company_name'] ?? '' }}</td>
+                            <td>{{ $company['company_founded_year'] ?? '' }}</td>
+                            <td>
+                                @if(!empty($company['company_logo']))
+                                    <img src="{{ asset($company['company_logo']) }}" alt="Company Logo" height="50" width="50" class="img-thumbnail" data-toggle="modal" data-target="#imageModal" data-image="{{ asset($company['company_logo']) }}">
                                 @else
-                                <span>unavailable</span>
-                                {{-- @endif --}}
-                            @endif
-                        </td>
-                        <td colspan="2">{{ !empty($legal_documents['pan']['pan_no']) ? $legal_documents['pan']['pan_no'] : '' }}</td>
-                        <td colspan="2">
-                            @if(!empty($legal_documents['pan']['image']))
-                            <img src="{{ asset($legal_documents['pan']['image']) }}" alt="PAN" class="img-thumbnail mt-2" height="50" width="50">
-                            @else
-                            <span>unavailable</span>
-                        @endif
-                        </td>
-                        <td colspan="2">{{ !empty($legal_documents['company']['register_no']) ? $legal_documents['company']['register_no'] : '' }}</td>
-                        <td colspan="2">
-                            @if(!empty($legal_documents['company']['register_file']))
-                                <img src="{{ asset($legal_documents['company']['register_file']) }}" alt="Register File" class="img-thumbnail mt-2" height="50" width="50">
+                                    <span>Unavailable</span>
+                                @endif
+                            </td>
+                            <td>{{ $legal_documents['pan']['pan_no'] ?? '' }}</td>
+                            <td>
+                                @if(!empty($legal_documents['pan']['image']))
+                                    <img src="{{ asset($legal_documents['pan']['image']) }}" alt="PAN" class="img-thumbnail mt-2" height="50" width="50" data-toggle="modal" data-target="#imageModal" data-image="{{ asset($legal_documents['pan']['image']) }}">
                                 @else
-                                <span>unavailable</span>
-                            @endif
-                        </td>
-                        <td colspan="2">
-                            @if(!empty($legal_documents['tax_clearance']))
-                                <img src="{{ asset($legal_documents['tax_clearance']) }}" alt="Tax Clearance" class="img-thumbnail mt-2" width="50" height="50">
+                                    <span>Unavailable</span>
+                                @endif
+                            </td>
+                            <td>{{ $legal_documents['company']['register_no'] ?? '' }}</td>
+                            <td>
+                                @if(!empty($legal_documents['company']['register_file']))
+                                    <img src="{{ asset($legal_documents['company']['register_file']) }}" alt="Register File" class="img-thumbnail mt-2" height="50" width="50" data-toggle="modal" data-target="#imageModal" data-image="{{ asset($legal_documents['company']['register_file']) }}">
                                 @else
-                                <span>unavailable</span>
-                            @endif
-                        </td>
-                        <td colspan="2"></td>
-                        <td colspan="2">
-                            {{-- <input type="checkbox" {{ $row->is_verified == 1 ? 'checked' : '' }}> --}}
-                            <label class="container">
-                                <input type="checkbox" {{ $row->is_verified == 1 ? 'checked' : '' }} class="is-verified" data-id="{{ $row->id }}">
-                                <span class="checkmark"></span>
-                            </label>
-                        </td>
-                        <td colspan="2">{{ Carbon\Carbon::parse($row->created_at)->format('Y-m-d') }}</td>
-                        <td colspan="2">
-                            <a href="{{ route('admin.users.edit', ['id' => $row->id])}}" data-original-title="Edit" data-toggle="tooltip" class="btn btn-warning btn-xs">
-                                <i class="fa fa-edit"></i>
-                            </a>
-                            <a href="{{ route('admin.users.show', ['id' => $row->id])}}" data-original-title="Show" data-toggle="tooltip" class="btn btn-warning btn-xs">
-                                <i class="fa fa-eye"></i>
-                            </a>
-                            <a href="{{ route('admin.admin_users.reset', ['id' => $row->id])}}" data-original-title="Reset password" data-toggle="tooltip" class="btn btn-warning btn-xs">
-                                <i class="fa fa-key"></i>
-                            </a>
-                            <a href="{{ route('admin.users.delete', ['id' => $row->id])}}" data-original-title="Delete" data-toggle="tooltip" class="btn btn-round btn-danger btn-xs" onClick="return confirm('Do you want to delete??')">
-                                <i class="fa fa-trash"></i>
-                            </a>
-                        </td>
-                    </tr>
-                    @endforeach
-                    @endif
+                                    <span>Unavailable</span>
+                                @endif
+                            </td>
+                            <td>
+                                @if(!empty($legal_documents['tax_clearance']))
+                                    <img src="{{ asset($legal_documents['tax_clearance']) }}" alt="Tax Clearance" class="img-thumbnail mt-2" width="50" height="50" data-toggle="modal" data-target="#imageModal" data-image="{{ asset($legal_documents['tax_clearance']) }}">
+                                @else
+                                    <span>Unavailable</span>
+                                @endif
+                            </td>
+                            <td>
+                                <label class="container">
+                                    <input type="checkbox" {{ $row->is_verified == 1 ? 'checked' : '' }} class="is-verified" data-id="{{ $row->id }}">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </td>
+                            <td>{{ $row->created_at->format('Y-m-d') }}</td>
+                            <td>
+                                <a href="{{ route('admin.users.edit', $row->id) }}" class="btn btn-warning btn-xs" data-toggle="tooltip" title="Edit">
+                                    <i class="fa fa-edit"></i>
+                                </a>
+                                <a href="{{ route('admin.users.show', $row->id) }}" class="btn btn-info btn-xs" data-toggle="tooltip" title="Show">
+                                    <i class="fa fa-eye"></i>
+                                </a>
+                                <a href="{{ route('admin.admin_users.reset', $row->id) }}" class="btn btn-primary btn-xs" data-toggle="tooltip" title="Reset Password">
+                                    <i class="fa fa-key"></i>
+                                </a>
+                                <a href="{{ route('admin.users.delete', $row->id) }}" class="btn btn-danger btn-xs" data-toggle="tooltip" title="Delete" onClick="return confirm('Do you want to delete?')">
+                                    <i class="fa fa-trash"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="18" class="text-center">No records found</td>
+                        </tr>
+                    @endforelse
                 </tbody>
             </table>
         </div>
     </div>
+
+    <!-- Image Modal -->
+    <div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="imageModalLabel">Image View</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body text-center">
+                    <img id="modal-image" src="" class="img-fluid" alt="Image Preview">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
 </div>
 
  <!-- Confirmation dialog -->
@@ -302,4 +315,13 @@
             });
         });
     </script>
+
+<script>
+    $('#imageModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var image = button.data('image');
+        var modal = $(this);
+        modal.find('#modal-image').attr('src', image);
+    });
+</script>
 @endsection
